85a09d18e0de5104f59af1f6c7850eda90f8b275
Trevor Roberts Jr <vmtrooper@gmail.com>
2016-03-02 13:21:33 -0800

    updates to workflows

--- Microsoft.PowerShellISE_profile.ps1
+++ Microsoft.PowerShellISE_profile.ps1
@@ -1,5 +1,6 @@
-. "C:\Program Files\VMware\Infrastructure\vSphere PowerCLI\Scripts\Initialize-PowerCLIEnvironment.ps1"
+#. "C:\Program Files\VMware\Infrastructure\vSphere PowerCLI\Scripts\Initialize-PowerCLIEnvironment.ps1"
 # Load posh-git example profile
+. (Resolve-Path "$env:ProgramFiles\VMware\Infrastructure\vSphere PowerCLI\Scripts\Initialize-PowerCLIEnvironment.ps1")
 . (Resolve-Path "$env:LOCALAPPDATA\GitHub\shell.ps1")
 . (Resolve-Path "$env:github_posh_git\profile.example.ps1")
 
--- list-vmware-guest-os-id.ps1
+++ list-vmware-guest-os-id.ps1
@@ -1,1 +1,19 @@
-﻿[System.Enum]::GetNames([VMware.Vim.VirtualMachineGuestOsIdentifier])
+﻿#[System.Enum]::GetNames([VMware.Vim.VirtualMachineGuestOsIdentifier])
+
+#New-VICredentialStoreItem -host prme-haas-vio-vcenter -user administrator@vsphere.local -password yourpassword`$
+#New-VICredentialStoreItem -host w2-haas01-vio-vcenter -user administrator@vsphere.local -password yourpassword`$
+
+$queryVM = get-vm -name windows2012-10*
+$queryVM.guest.GuestId
+get-harddisk -vm $queryvm |format-list
+$queryVM | Get-ScsiController
+
+# First, identify which VM you are importing to OpenStack
+$OpenStackWinVM = get-vm -name windows2012-10*
+# Now, get the Operating System ID for the vmware_ostype property
+$OpenStackWinVM.guest.GuestId
+# We also need the SCSI controller type for the vmware_adaptertype property
+$OpenStackWinVM | Get-ScsiController
+# Finally, get the disk type for the vmware_disktype property
+get-harddisk -vm $OpenStackWinVM |format-list
+
